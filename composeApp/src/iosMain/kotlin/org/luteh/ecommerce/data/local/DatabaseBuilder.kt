16124ba2a1245875d4eba7592b15a6c94a481361
Luthfan Maftuh <luthfanmaftuh@gmail.com>
2026-01-03 18:45:34 +0700

    Fix ios's room database initialization

--- composeApp/src/iosMain/kotlin/org/luteh/ecommerce/data/local/DatabaseBuilder.kt
+++ composeApp/src/iosMain/kotlin/org/luteh/ecommerce/data/local/DatabaseBuilder.kt
@@ -2,13 +2,15 @@ package org.luteh.ecommerce.data.local
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import platform.Foundation.NSHomeDirectory
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
     val dbFilePath = NSHomeDirectory() + "/luteh_ecommerce.db"
     return Room.databaseBuilder<AppDatabase>(
             name = dbFilePath,
-            factory = { AppDatabase::class.instantiateImpl() },
+            factory = { AppDatabaseConstructor.initialize() },
         )
+        .setDriver(BundledSQLiteDriver())
         .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
 }
